88d1b314eb200060add10ed47c2da40982fd8c67
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-05-01 19:54:39 +0900

    nginx をインストール

--- chef-repo/cookbooks/nginx/recipes/default.rb
+++ chef-repo/cookbooks/nginx/recipes/default.rb
@@ -6,3 +6,27 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package 'nginx' do
+    action [ :install ]
+    options "--enablerepo=epel"
+end
+
+%w{
+    nginx
+    iptables
+}.each do |svc|
+    service svc do
+      supports [ :restart, :reload ]
+      action [ :enable, :start ]
+    end
+end
+
+iptables_cons_path = '/etc/sysconfig/iptables'
+allow_port = 80
+execute "allow port #{allow_port}" do
+    not_if "grep '-A INPUT -p tcp -m state --state NEW -m tcp --dport #{allow_port}' #{iptables_conf_path}"
+    command "sed -i -e '/:OUTPUT ACCEPT/a-A INPUT -p tcp -m state --state NEW -m tcp --dport #{allow_port} -j ACCEPT' #{iptables_conf_path}"
+    action :run
+    notifies :restart, "service[iptables]"
+end
