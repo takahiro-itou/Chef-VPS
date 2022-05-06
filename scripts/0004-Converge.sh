fd7a9f49b6dd3687b2d641fa81a3b1aa3d1e4443
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-05-06 14:39:37 +0900

    リモートサーバー側で Chef を実行するスクリプトを追加

---
+++ scripts/0004-Converge.sh
@@ -0,0 +1,32 @@
+#!/bin/bash  -xue
+
+script_dir=$(dirname $0)
+chef_dir=${script_dir}/../chef-repo
+
+function  show_help_message () {
+    echo  "Usage: $0 (KEY) [USER]" 1>&2
+}
+
+if [[ $# -lt 1 ]] ; then
+    show_help_message
+    exit  2
+fi
+
+ssh_private_key=$1
+shift  1
+
+ssh_user_name=ubuntu
+if [[ $# -ge 1 ]] ; then
+    ssh_user_name=$1
+    shift  1
+fi
+
+pushd  ${chef_dir}
+knife zero -c conf.rb               \
+    converge  'name:server1'        \
+    -a knife_zero.host              \
+    -x ${ssh_user_name}             \
+    -i ${ssh_private_key}           \
+    --sudo  --verbose               \
+    ;
+popd
